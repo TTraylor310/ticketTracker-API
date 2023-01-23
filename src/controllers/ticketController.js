88366f2a50869d3cc31b2e89eaa7f669b6534a13
Tim Traylor <ttraylor83@gmail.com>
2023-01-23 23:05:42 +0000

    ticket stuff completed, notes next

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -81,5 +81,5 @@ module.exports = {
   getTickets,
   getTicket,
   deleteTicket,
-  
+  updateTicket,
 }
--- src/routes/ticketRoutes.js
+++ src/routes/ticketRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
 const { protect } = require('../middleware/authMiddleware')
 const {
   createTicket,
   getTickets,
   getTicket,
   deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController')
+
+// re-route into note Routers
 
 router.route('/')
   .get(protect, getTickets)
@@ -15,5 +17,6 @@ router.route('/')
 router.route('/:id')
   .get(protect, getTicket)
   .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
 
 module.exports = router
